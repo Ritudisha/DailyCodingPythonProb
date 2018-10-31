9e85a07f11a798923e0f53b6d3670f113d6901a7
Ritudisha <ritudishabiswas@gmail.com>
2018-10-31 15:58:52 +0530

    Update PrimeMinister's Number.py

--- PythonCodes/PrimeMinister's Number.py
+++ PythonCodes/PrimeMinister's Number.py
@@ -1,15 +1,5 @@
-'''
-# Sample code to perform I/O:
-
-name = input()                  # Reading input from STDIN
-print('Hi, %s.' % name)         # Writing output to STDOUT
-
-# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
-'''
-
-# Write your code here
 import math
-# Write your code here
+
 n = input()
 a = n.split(' ')
 result = ""
